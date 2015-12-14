37f380a745e10c32dea126e45c9ac34f152ada75
rasmus paetau <rasmuspaetau@hotmail.com>
2015-12-14 11:33:27 +0100

    done!!

--- FitnessApp/FitnessApp/routes/exerciseProgram.js
+++ FitnessApp/FitnessApp/routes/exerciseProgram.js
@@ -6,6 +6,7 @@ var ObjectId = require('mongodb').ObjectID;
 
 var mongoUri = 'mongodb://localhost:27017/test';
 var stuff = [];
+var exercisePrograms = [];
 /* GET home page. */
 router.get('/', function (req, res) {
     
@@ -20,9 +21,30 @@ router.get('/', function (req, res) {
             } else {
                 db.close();
                 
-                
-                res.render('exerciseProgram', { stuff: stuff });
-                stuff = [];
+                mongoClient.connect(mongoUri, function (err, db) {
+                    assert.equal(null, err);
+                    var cursor = db.collection('exercisePrograms').find();
+                    cursor.each(function (err, doc) {
+                        assert.equal(err, null);
+                        if (doc != null) {
+                            exercisePrograms.push(doc);
+                        } else {
+                            db.close();
+                            
+                            
+                            
+                            
+                            
+                            res.render('exerciseProgram', { stuff: stuff, exercisePrograms : exercisePrograms });
+                            stuff = [];
+                            exercisePrograms = [];
+                        }
+                    });
+
+                });
+
+
+
             }
         });
 
@@ -34,10 +56,28 @@ router.get('/', function (req, res) {
 router.post('/', function (req, res) {
     //get form info and save it.    
     var data = req.body;
-    
+    var obj = {}
+    obj.program = [];
 
-    
-    res.redirect('exercise');
+    var keyNames = Object.keys(data);
+    keyNames.forEach(function(item) {
+        obj.program.push(item);
+    });
+
+    mongoClient.connect(mongoUri, function (err, db) {  
+        assert.equal(null, err);
+        console.log("Connected to mongodb");
+        db.collection('exercisePrograms').insertOne(obj, function (err, result) {
+            assert.equal(err, null);
+            console.log("Inserted a document into the exercisePrograms collection.");
+            db.close();
+            obj = null;
+            
+            res.redirect('exerciseProgram');
+        });
+    });
+
+
 });
 
 module.exports = router;
